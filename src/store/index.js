4d76893bd9f119c5f823b26a9ced19eeddf1d256
jasonpa301 <jasonpa301@gmail.com>
2022-01-04 14:48:49 +0200

    display all readings, filter by farm

--- src/store/index.js
+++ src/store/index.js
@@ -5,13 +5,58 @@ const backendURL = process.env.VUE_APP_BACKEND_URL;
 const app = {
   namespaced: true,
   state: {
-    
+    readings: null,
+    filteredReadings: null,
+    farms: null
   },
   mutations: {
-    
+    updateReadings(state, readings) {
+      state.readings = readings;
+      state.filteredReadings = readings;
+    },
+    updateFarms(state, readings) {
+      state.farms = readings;
+    },
+    filterReadingsByFarm(state, farmId) {
+      if (farmId == 0) {
+        state.filteredReadings = state.readings;
+      }
+      else {
+        var readings = [];
+        state.readings.forEach((element) => { 
+          if (element.farm_id == farmId) {
+            readings.push(element);
+          }
+        })
+        state.filteredReadings = readings;
+      }
+    }
   },
   actions: {
-    
+    async getAllReadings(context) {
+      try {
+        const url = backendURL + "api/readings/all";
+        var {data} = await axios.get(url);
+        console.log(data);
+        if (data) {
+          context.commit("updateReadings", data);
+        }
+      } catch (e) {
+        throw new Error(e);
+      }
+    },
+    async getAllFarms(context) {
+      try {
+        const url = backendURL + "api/farms/all";
+        var {data} = await axios.get(url);
+        console.log(data);
+        if (data) {
+          context.commit("updateFarms", data);
+        }
+      } catch (e) {
+        throw new Error(e);
+      }
+    }
   },
   getters: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,65 @@
 <template>
   <div class="home">
+    <h1>Farms</h1>
+    <div class="reading-filters">
+      <h2>Filters</h2>
+      <label>Select farm: </label> 
+      <select v-model="selectedFarm" @change="filterByFarm">
+        <option :value="0" selected>All</option>
+        <option v-for="(farm, index) in farms" :key="index" :value="farm.id">{{farm.farm_name}}</option>
+      </select>
+    </div>
+    <div class="table-container">
+      <table id="readings-table">
+        <thead>
+          <tr>
+            <th>Farm name</th>
+            <th>Time</th>
+            <th>Reading Type</th>
+            <th>Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(row, index) in readings" :key="index">
+            <td>{{row.farm_name}}</td>
+            <td>{{row.date_time}}</td>
+            <td>{{row.reading_type}}</td>
+            <td>{{row.reading_value}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
     
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
 
 export default {
   name: 'Home',
-  components: {
-    
+  computed: {
+    ...mapState({
+      readings: (state) => state.app.filteredReadings,
+      farms: (state) => state.app.farms,
+    }),
   },
-  mounted() {
-
+  data() {
+    return {
+      selectedFarm: 0,
+    };
+  },
+  async mounted() {
+    const self = this;
+    await self.$store.dispatch("app/getAllReadings");
+    await self.$store.dispatch("app/getAllFarms");
+  },
+  methods: {
+    filterByFarm() {
+      const self = this;
+      console.log(self.$store);
+      self.$store.commit("app/filterReadingsByFarm",self.selectedFarm);
+    }
   }
 }
 </script>
