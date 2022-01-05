8a45671fb0cd1416a806657d13f6a8acd8f639f7
jasonpa301 <jasonpa301@gmail.com>
2022-01-05 15:52:23 +0200

    filter readings by type and date

--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,8 @@ const app = {
   state: {
     readings: null,
     filteredReadings: null,
-    farms: null
+    farms: null,
+    readingTypes: null
   },
   mutations: {
     updateReadings(state, readings) {
@@ -17,19 +18,44 @@ const app = {
     updateFarms(state, readings) {
       state.farms = readings;
     },
-    filterReadingsByFarm(state, farmId) {
-      if (farmId == 0) {
+    filterReadings(state, filterObject) {
+      var farmId = filterObject.farmId;
+      var readingTypeId = filterObject.readingTypeId;
+      var startDate = filterObject.startDate;
+      var endDate = filterObject.endDate;
+
+      if (farmId == 0 && readingTypeId == 0 && !startDate && !endDate) {
         state.filteredReadings = state.readings;
       }
       else {
         var readings = [];
+        var add;
         state.readings.forEach((element) => { 
-          if (element.farm_id == farmId) {
+          add = true;
+          if (farmId != 0 && element.farm_id != farmId) {
+            add = false;
+          }
+          if (readingTypeId != 0 && element.reading_type_id != readingTypeId) {
+            add = false;
+          }
+
+          if (startDate && element.date_time < startDate) {
+            add = false;
+          }
+
+          if (endDate && element.date_time >= endDate) {
+            add = false;
+          }
+
+          if (add == true) {
             readings.push(element);
           }
         })
         state.filteredReadings = readings;
       }
+    },
+    updateReadingTypes(state, readingTypes) {
+      state.readingTypes = readingTypes;
     }
   },
   actions: {
@@ -52,6 +78,18 @@ const app = {
         console.log(data);
         if (data) {
           context.commit("updateFarms", data);
+        }
+      } catch (e) {
+        throw new Error(e);
+      }
+    },
+    async getAllReadingTypes(context) {
+      try {
+        const url = backendURL + "api/readingtypes/all";
+        var {data} = await axios.get(url);
+        console.log(data);
+        if (data) {
+          context.commit("updateReadingTypes", data);
         }
       } catch (e) {
         throw new Error(e);
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,12 +3,35 @@
     <h1>Farms</h1>
     <div class="reading-filters">
       <h2>Filters</h2>
-      <label>Select farm: </label> 
-      <select v-model="selectedFarm" @change="filterByFarm">
-        <option :value="0" selected>All</option>
-        <option v-for="(farm, index) in farms" :key="index" :value="farm.id">{{farm.farm_name}}</option>
-      </select>
+      <div class="filter-input-container">
+        <label>Select farm: </label> 
+        <select v-model="selectedFarm" @change="filterReadings">
+          <option :value="0" selected>All</option>
+          <option v-for="(farm, index) in farms" :key="index" :value="farm.id">{{farm.farm_name}}</option>
+        </select>
+      </div>
+      
+      <div class="filter-input-container">
+        <label>Select Reading Type: </label> 
+        <select v-model="selectedReadingType" @change="filterReadings">
+          <option :value="0" selected>All</option>
+          <option v-for="(readingType, index) in readingTypes" :key="index" :value="readingType.id">{{readingType.reading_type}}</option>
+        </select>
+      </div>
+
+      <div class="filter-input-container">
+        <label>Select start date: </label>
+        <input type="date" v-model="startDate" @input="filterReadings"/>
+      </div>
+
+      <div class="filter-input-container">
+        <label>Select end date: </label>
+        <input type="date" v-model="endDate" @input="filterReadings"/>
+      </div>
+
+    
     </div>
+
     <div class="table-container">
       <table id="readings-table">
         <thead>
@@ -35,6 +58,7 @@
 
 <script>
 import { mapState } from "vuex";
+import moment from 'moment';
 
 export default {
   name: 'Home',
@@ -42,23 +66,36 @@ export default {
     ...mapState({
       readings: (state) => state.app.filteredReadings,
       farms: (state) => state.app.farms,
+      readingTypes: (state) => state.app.readingTypes
     }),
   },
   data() {
     return {
       selectedFarm: 0,
+      selectedReadingType: 0,
+      startDate: "",
+      endDate: ""
     };
   },
   async mounted() {
     const self = this;
     await self.$store.dispatch("app/getAllReadings");
     await self.$store.dispatch("app/getAllFarms");
+    await self.$store.dispatch("app/getAllReadingTypes");
   },
   methods: {
-    filterByFarm() {
+    filterReadings() {
       const self = this;
-      console.log(self.$store);
-      self.$store.commit("app/filterReadingsByFarm",self.selectedFarm);
+      var startDate = "";
+      if (self.startDate) {
+        startDate = moment(self.startDate).format('Y-MM-DD HH:mm:ss')
+      }
+      var endDate = "";
+      if (self.endDate) {
+        var formatEndDate = moment(self.endDate, 'Y-MM-DD').add(1,'days');
+        endDate = moment(formatEndDate).format('Y-MM-DD HH:mm:ss');
+      }
+      self.$store.commit("app/filterReadings", {farmId: self.selectedFarm, readingTypeId: self.selectedReadingType, startDate: startDate, endDate: endDate });
     }
   }
 }
